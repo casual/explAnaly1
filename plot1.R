985c8be35c375157f38742b477df766f1ce4ef74
casual <1cgajardo2009@gmail.com>
2016-04-10 14:36:29 -0300

    First plot .R and .png

---
+++ plot1.R
@@ -0,0 +1,12 @@
+##Plot 1
+
+##First we load the subset from the data
+
+info<-readRDS("subsetData.r")
+
+##Now we plot and save in png file
+
+png("plot1.png")
+par(mar = c(4,4,1,1))
+hist(info$Global_active_power, xlab = "Global Active Power (kilowatts)", col = "red", main = "Global Active Power", breaks = 11, ylim = c(0,1200))
+dev.off()
