c0b269d6d132e5b7782811a9d61ba799d0e32a7d
casual <1cgajardo2009@gmail.com>
2016-04-10 15:25:37 -0300

    subset code updated to modify the Data variable

--- subset_code.R
+++ subset_code.R
@@ -3,9 +3,14 @@
 
 info<-read.table("household_power_consumption.txt",header=FALSE, skip = 66637, nrow=2882, sep=";")
 
-##then we name the variables
+##Then we name the variables
 info_names<-read.table("household_power_consumption.txt",header=FALSE, nrow=1, sep=";",colClasses = c("character","character","character","character","character","character","character","character","character"))
 colnames(info)<-info_names
+
+##Now we transform the "Date" variable
+
+info$Date<-as.Date(as.character(info$Date),"%d/%m/%Y")
+info$Date<-weekdays(info$Date)
 
 ##We save the R object to use it later to plot
 saveRDS(info,file="subsetData.R")
